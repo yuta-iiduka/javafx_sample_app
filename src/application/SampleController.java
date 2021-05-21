4253e87bf3f08de691665e4880068a924612f614
ota124 <ota12041204@gmail.com>
2021-05-21 19:15:25 +0900

    add label

--- src/application/SampleController.java
+++ src/application/SampleController.java
@@ -1,5 +1,25 @@
 package application;
 
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
 public class SampleController {
-	
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private Label lbl;
+
+    @FXML
+    void initialize() {
+        assert lbl != null : "fx:id=\"lbl\" was not injected: check your FXML file 'Sample.fxml'.";
+        lbl.setText("Hello JavaFx");
+
+    }
 }
